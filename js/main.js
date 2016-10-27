6d30c4a4ac0b0ef437e2a1b56b84bd560e7298c1
JocelynJoe <jocelyn.pena.contreras@gmail.com>
2016-10-27 13:30:34 -0700

    organizcion de funciones y validacion de lista

--- js/main.js
+++ js/main.js
@@ -1,46 +1,63 @@
 function validateForm(){
+
+
     //validar nombre
     var name=document.getElementById('name').value;
     if(name.length==0){
         alert("Debe ingresar su nombre");
         return false;
     }
-    validarLetras(name);
-    validarMayus(name);
-    //valida que sea solo letras 
-    function validarLetras(nombre){
-        var filter6=/^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
-        if (filter6.test(nombre)){
-            return true;
-        }else{
-         alert("Debe Ingresar solo letras");
-        } 
-    }
-    //valida primera con mayuscula
-    function validarMayus(nombre){
-        if(nombre.substring(0,1)==nombre.substring(0,1).toUpperCase()){
-            return true;
-        }else{
-            alert("Debe comenzar con Mayuscula");
-            return false;
-        }
-    }
-
-    
+   
+  
     //validar apellido
     var lastName=document.getElementById('lastname').value;
     if(lastName.length==0){
         alert("Debe ingresar su Apellido");
         return false;
     }
+    
+    validarLetras(name);
+    validarMayus(name);
     validarLetras(lastName);
     validarMayus(lastName);
-    
-
+    validarPass();
+    validarMail();
+    validarLista();
   
+}
+function validarMayus(nombre){
+        if(nombre.substring(0,1)==nombre.substring(0,1).toUpperCase()){
+            return true;
+        }else{
+            alert("Debe comenzar con Mayuscula");
+            return false;
+        }
+}
+function validarLetras(nombre){
+        var filter6=/^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
+        if (filter6.test(nombre)){
+            return true;
+        }else{
+         alert("Debe Ingresar solo letras");
+        } 
+}
+function validarMail(){
+    var mail = document.getElementById("input-email").value;
 
-    //validar password
-    var pass=document.getElementById('input-password').value;
+    if(mail.length==0){
+        alert("Debe ingresar su mail");
+        return false;
+    }
+
+    if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(mail)) ) {
+        return false;
+
+    }
+
+}
+
+function validarPass(){
+     var pass=document.getElementById('input-password').value;
     //campo obligatorio
     if(pass.length==0){
         alert("Falta Ingresar Password");
@@ -56,29 +73,12 @@ function validateForm(){
     }else{
         alert("Su contraseña no puede ser ni 123456 ni 098754");
     }
-    
 
-    //validar email
-    var mail = document.getElementById("input-email").value;
-
-    if(mail.length==0){
-        alert("Debe ingresar su mail");
-        return false;
-    }
-
-    if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(mail)) ) {
-        return false;
-
-    }
-
-    //validar lista pendiente
-  
-    var opcion = document.getElementsByTagName("select")[0].value;
-    if(opcion == "0") {
+}
+function validarLista(){
+    var opcion = document.querySelector("select").selectedIndex;
+    if(opcion == "0" || opcion==null) {
         alert("Elija una opcion de la lista");
     }
-    /*if(0 == document.getElementsByTagName('select')[0].selectedIndex){
-        alert("Elija una opcion de la lista"); 
-    }*/
-  
 }
+
